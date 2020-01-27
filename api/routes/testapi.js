163efb3a7a70e79d0800c67a95c53613f31e52b4
matt benedix <matt.benedix@gmail.com>
2020-01-27 08:22:48 -0500

    save data from client to db

--- api/routes/testapi.js
+++ api/routes/testapi.js
@@ -24,4 +24,21 @@ router.get('/', function(req, res, next) {
   
 });
 
+router.post('/', (req, res, next) => {
+ 
+  let reqPerson = req.body;
+  let newName = reqPerson.name;
+  let newAge = reqPerson.age; 
+
+  let newPerson = new Person({name: newName, age: newAge, favoriteFoods: ["pizza"] });
+
+  newPerson.save((err, p) => {
+    if (err) return console.error(err);
+    console.log(p.name + " saved to database brooo");
+    //res.send(p.name + " saved to database brooo");
+    res.json(p);
+  })
+  
+})
+
 module.exports = router;
