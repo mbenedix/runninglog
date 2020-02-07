0882b6bafa017b65e019c612cf612a66181baf89
matt benedix <matt.benedix@gmail.com>
2020-02-06 21:26:14 -0500

    jwt validation and decoding functions

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,24 +1,51 @@
 const JSRSASign = require("jsrsasign");
 
+const key = process.env.JWTKEY; 
+
 const generateJWT = () => {
-const claims = {
-  Username: "praveen",
-  Age: 27,
-  Fullname: "Praveen Kumar"
+  const claims = {
+    Username: "praveen",
+    Age: 27,
+    Fullname: "Praveen Kumar"
+  }
+  const key = process.env.JWTKEY; 
+  const header = {
+    alg: "HS512",
+    typ: "JWT"
+  };
+
+  var sHeader = JSON.stringify(header);
+  var sPayload = JSON.stringify(claims);
+
+  const sJWT = JSRSASign.jws.JWS.sign("HS512", sHeader, sPayload, key);
+  
+  decodeJWT(sJWT);
+  return sJWT; 
 }
 
-const key = "$PraveenIsAwesome!"; //*** PUT IN ENV FILE */
+const validateJWT = (token) => {
+  const algorithm = "HS512";
+  const key = process.env.JWTKEY; 
 
-const header = {
-  alg: "HS512",
-  typ: "JWT"
-};
+  return JSRSASign.jws.JWS.verifyJWT(token, key, {
+    alg: [algorithm]
+  });
 
-var sHeader = JSON.stringify(header);
-var sPayload = JSON.stringify(claims);
-
-const sJWT = JSRSASign.jws.JWS.sign("HS512", sHeader, sPayload, key);
-console.log(sJWT);
 }
+
+const decodeJWT = (token) => {
+  aToken = token.split(".");
+  const uHeader = JSRSASign.b64utos(aToken[0]);
+  const uClaim = JSRSASign.b64utos(aToken[1]);
+  const pHeader = JSRSASign.jws.JWS.readSafeJSONString(uHeader);
+  const pClaim = JSRSASign.jws.JWS.readSafeJSONString(uClaim);
+
+  return pClaim;
+  console.log(pHeader);
+  console.log(pClaim);
+}
+
+
 
 exports.generateJWT = generateJWT; 
+exports.validateJWT = validateJWT;
