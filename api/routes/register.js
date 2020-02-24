02a3aa3fdc7e9e9f1ffe81999aad40af630c04a4
matt benedix <matt.benedix@gmail.com>
2020-02-24 07:35:32 -0500

    fixed bug with detecting existing usernames

--- api/routes/register.js
+++ api/routes/register.js
@@ -5,27 +5,33 @@ const bcrypt = require("bcrypt");
 
 User = dbs.user;
 
+
 router.post('/', (req, res, next) => {
   const newUser = new User({ 
       username: req.body.username, 
       password: bcrypt.hashSync(req.body.password, 12),
       email: req.body.email 
   });
+  
+  User.exists({ username: req.body.username }, function (err, exists) {
+    if (err) {
+      res.send("user registration failed");
+    }
 
-  if(User.exists({ username: req.body.username })) {
-    res.send("user already exists")
-  }
-
-  else {
-    newUser.save(err => {
-      if (err) {
-        res.send("user registration failed");
-      } 
-      else {
-        res.send("successful user registration");
-      }
-    });
-  }
+    else if(exists) {
+      res.send("user already exists");
+    }
+    else {
+      newUser.save(err => {
+        if (err) {
+          res.send("user registration failed");
+        } 
+        else {
+          res.send("successful user registration");
+        }
+      });
+    } 
+  });
 });
 
 module.exports = router;
